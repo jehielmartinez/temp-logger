29c946924c92cd58f164b34d49a15988a1357021
Jehiel Martinez <jehielmartinez@gmail.com>
2018-11-19 01:02:26 -0600

    add reactapp for client

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const connectionString = process.env.DATABASE_URI || 'postgresql://postgres:josu
 const pool = new pg.Pool({ connectionString });
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 
 app.use(bodyParser.json());
