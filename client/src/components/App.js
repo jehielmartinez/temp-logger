9bf7be8b11d78c5cbc4a4d5e7a1a9ad2146d8384
Jehiel Martinez <jehielmartinez@gmail.com>
2018-11-19 02:42:28 -0600

    socket added handshake done!

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Chart from "./Chart";
+import socketIOClient from 'socket.io-client';
 
 class App extends Component {
+  state = {
+    endpoint: `${window.location.hostname}:4000`,
+    data: {}
+  }
+
   render() {
+    const socket = socketIOClient(this.state.endpoint);
+
+    socket.on('newLog', (log) => {
+      console.log(log);
+      this.setState({data: log})
+    })
+
     return (
       <div>
         <Header title="TEMPERATURE MONITOR" />
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const pg = require('pg');
 
@@ -8,11 +10,16 @@ const pool = new pg.Pool({ connectionString });
 
 const app = express();
 const port = process.env.PORT || 4000;
+const server = http.createServer(app);
 
+const io = socketIO(server);
 
 app.use(bodyParser.json());
 
-pool.connect()
+io.on('connection', (socket) => {
+    console.log('Client Connected');
+
+    pool.connect()
     .then((client) => {
         return client.query('CREATE TABLE log(id SERIAL PRIMARY KEY, device VARCHAR(40) not null, created_on VARCHAR(50) not null, metric VARCHAR(50))')
         .then((res)=>{
@@ -22,22 +29,27 @@ pool.connect()
         console.error('Base de Datos ya existe', err);
     });
 
-app.post('/log', (req, res) => {
-    data = [req.query.device, new Date().valueOf(), req.query.metric];
-
-    pool.connect()
-        .then((client) => {
-            return client.query(
-                    'INSERT INTO log(device, created_on, metric) VALUES($1, $2, $3) RETURNING *',
-                    data)
-                    .then((response)=>{
-                        client.release();
-                        res.status(200).json({success: true, data: response.rows[0]})
-                    })
-        .catch((err) => {
-            console.log(err);
+    app.post('/log', (req, res) => {
+        data = [req.query.device, new Date().valueOf(), req.query.metric];
+    
+        pool.connect()
+            .then((client) => {
+                return client.query(
+                        'INSERT INTO log(device, created_on, metric) VALUES($1, $2, $3) RETURNING *',
+                        data)
+                        .then((response)=>{
+                            client.release();
+                            res.status(200).json({success: true, data: response.rows[0]})
+                            io.emit('newLog', response.rows[0]);
+                        })
+            .catch((err) => {
+                console.log(err);
+            })
         })
     })
 })
 
-app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+
+
+
+server.listen(port, () => console.log(`listening on http://localhost:${port}`));
