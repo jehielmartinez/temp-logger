7ea4c4b222bdf0b0b95305bce9d812986389e093
Jehiel Martinez <jehielmartinez@gmail.com>
2018-11-21 05:15:30 -0600

    response as array

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -54,7 +54,7 @@ class App extends Component {
     fetch(url, { method: "GET" })
       .then(res => {
         let responseData = res.json();
-        if (responseData === "[]"){
+        if (responseData === []){
           this.setState({error: true})
         } else {
           return responseData;
